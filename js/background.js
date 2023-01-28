c3798b279c6ed3afb99ccbfa5fa5e1d8f7aa9054
Yakumo Saki <yakumo@ziomatrix.org>
2023-01-28 16:31:53 +0900

    manifest v3

--- js/background.js
+++ js/background.js
@@ -1,46 +1,50 @@
 "use strict";
 
-console.log( 'Kurotodon background loaded.' );
+function createOrFocusTab() {
+    chrome.windows.getAll({ populate: true }, function (wins) {
+        let multi = false;
+        let focuswin = null;
 
-chrome.browserAction.onClicked.addListener( function( tab ) {
-	chrome.windows.getAll( { populate: true }, function( wins ) {
-		var multi = false;
-		var focuswin = null;
+        for (let i = 0, _len = wins.length; i < _len; i++) {
+            if (wins[i].focused) {
+                focuswin = wins[i];
+            }
 
-		for ( var i = 0, _len = wins.length ; i < _len ; i++ )
-		{
-			if ( wins[i].focused )
-			{
-				focuswin = wins[i];
-			}
+            for (let j = 0, __len = wins[i].tabs.length; j < __len; j++) {
+                if (
+                    wins[i].tabs[j].url.match(/^chrome-extension:\/\//) &&
+                    wins[i].tabs[j].title == "Kurotodon財団版"
+                ) {
+                    // 多重起動
+                    multi = true;
 
-			for ( var j = 0, __len = wins[i].tabs.length ; j < __len ; j++ )
-			{
-				if ( wins[i].tabs[j].url.match( /^chrome-extension:\/\// ) &&
-					 wins[i].tabs[j].title == 'Kurotodon' )
-				{
-					// 多重起動
-					multi = true;
+                    // 既に開いているウィンドウ＆タブにフォーカス
+                    chrome.windows.update(wins[i].id, { focused: true });
+                    chrome.tabs.update(wins[i].tabs[j].id, { selected: true });
 
-					// 既に開いているウィンドウ＆タブにフォーカス
-					chrome.windows.update( wins[i].id, { focused: true } );
-					chrome.tabs.update( wins[i].tabs[j].id, { selected: true } );
+                    break;
+                }
+            }
+        }
 
-					break;
-				}
-			}
-		}
+        if (multi == false) {
+            let param = { "url": "index.html" };
 
-		if ( multi == false )
-		{
-			var param = { url: chrome.extension.getURL( 'index.html' ) };
+            if (focuswin != null) {
+                param.windowId = focuswin.id;
+            }
 
-			if ( focuswin != null )
-			{
-				param.windowId = focuswin.id;
-			}
+            chrome.tabs.create(param);
+        }
+    });
+}
 
-			chrome.tabs.create( param );
-		}
-	} );
-} );
+chrome.action.onClicked.addListener((tab) => {
+    console.log("test");
+    createOrFocusTab();
+    // console.log(chrome.action);
+
+    // chrome.scripting.executeScript({
+    // 	function: createOrFocusTab
+    // })
+});
